749b2c9b42557af2c3e1c6411498177d8f23f694
Ally Jarjour <allyjarjour@gmail.com>
2020-05-04 21:22:09 -0600

    Build first phase of components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+// import technology from '../../data/technology';
+// import science from '../../data/technology';
+// import health from '../../data/health';
+// import entertainment from '../../data/entertainment';
 import './App.css';
+import NewsContainer from '../NewsContainer/NewsContainer'
+import Menu from '../Menu/Menu'
 
 class App extends Component {
   constructor() {
@@ -10,10 +16,16 @@ class App extends Component {
     }
   }
 
+  handleSelection() {
+    console.log('hi');
+    
+  }
+
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <Menu onChange={this.handleSelection} />
+        <NewsContainer allData={this.state.local}/>
       </div>
     );
   }
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle.js'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+const NewsContainer = (props) => {
+    return (
+        <section className="news-container">
+            {props.allData.map(prop => {
+                return <NewsArticle 
+                    key={prop.id}
+                    articleID={prop.id} 
+                    headline={prop.headline} 
+                    imgURL={prop.img} 
+                    description={prop.description} 
+                    articleURL={prop.url} />
+            })}
+        </section>
+    );
+}
 
 export default NewsContainer;
