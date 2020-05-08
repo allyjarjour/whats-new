c3fc18c6b8cf38c1a98a3ac42477b3ad159f05b1
Ally Jarjour <allyjarjour@gmail.com>
2020-05-08 10:49:57 -0600

    Updates

---
+++ precache-manifest.9482b506e8394527d9bc7e2f6350fa71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ae89ba24d4c7835a802633d7b16802",
+    "url": "/whats-new/index.html"
+  },
+  {
+    "revision": "1063e5482e2b0fbceffe",
+    "url": "/whats-new/static/css/main.33573bb4.chunk.css"
+  },
+  {
+    "revision": "9cb6184a6fce5e608afe",
+    "url": "/whats-new/static/js/2.2a8b2e1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whats-new/static/js/2.2a8b2e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1063e5482e2b0fbceffe",
+    "url": "/whats-new/static/js/main.b521f0e1.chunk.js"
+  },
+  {
+    "revision": "deccfd15b8e90df9b470",
+    "url": "/whats-new/static/js/runtime-main.fe69efb1.js"
+  }
+]);
